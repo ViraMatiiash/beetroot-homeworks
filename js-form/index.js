5bb550871e40f301701417832d18928b38cde8be
viramatiiash <virko1996@gmail.com>
2022-05-06 10:36:34 +0300

    finished js-form homework

---
+++ js-form/index.js
@@ -0,0 +1,3 @@
+import { registration } from './js/registration.js';
+
+document.getElementById('js-form').addEventListener('submit', registration);
---
+++ js-form/js/registration.js
@@ -0,0 +1,29 @@
+const showError = (id, error) => {
+  const element = document.getElementById(id);
+  error ? element.classList.add('error') : element.classList.remove('error');
+};
+
+const registration = (event) => {
+  event.preventDefault();
+  console.log(event);
+  const form = new FormData(event.target);
+  const firstName = form.get('firstName');
+  console.log(firstName);
+  const lastName = form.get('lastName');
+  console.log(lastName);
+  const email = form.get('email');
+  console.log(email);
+  const phone = form.get('phone');
+  console.log(phone);
+
+  const generateId = (name) => {
+    return `js-form-${name}`;
+  };
+
+  showError(generateId('first-name'), !firstName);
+  showError(generateId('last-name'), !lastName);
+  showError(generateId('email'), !email);
+  showError(generateId('phone'), !phone);
+};
+
+export { registration };
